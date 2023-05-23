cf14d548d43fe716d6eff4a0ae192f9c37427d7e
MOUHCINEASMAA <133967193+MOUHCINEASMAA@users.noreply.github.com>
2023-05-23 11:37:46 +0000

    const element = array[index];                 condition
                          do {
                               }););"KABBAJ,"KABBAJ"],
                                  } while (for (let for (const key in object) {
                                      if (Object.hasOwnProperty.call(object, key)) {
                                              const element = object[key];
                                                      array
                                                          }

---
+++ family Appointment scripts/var lastNames =["MOUHCINE","MOUHCINE","K.js
@@ -0,0 +1,9 @@
+    var lastNames =["MOUHCINE","MOUHCINE","KABBAJ"],
+            firstNames =["SAMIRA","ABDESSAMAD","MOHAMED ADAM"],
+                    births =["1964-01-19","09-09-1997","2006-08-09"],
+                            passNumbers =["YO9246071","LF0000000","SD7920506"],
+                                    issueDates =["2023-02-22","2019-02-12","2023-02-27"],
+                                            expiryDates =["2028-02-22","2024-02-12","2028-02-27"],
+                                                    pptissuePalaces =["CASABLANCA","CASABLANCA","CASABLANCA"],
+                                                            number = 3,
+                                                                  //########################################################################
